42e01f6d88da27ac064fff0e8ccfddd06bbcb3b9
Ravinder-kumar95 <ravinderkumar95@outlook.com>
2016-02-06 21:48:49 +0530

    06FEB
    added modules

--- sales.js
+++ sales.js
@@ -7,3 +7,13 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
 }
+
+function bigimg(x) {
+    x.style.width = "50%";
+    x.style.height = "35%";
+}
+
+function normalimg(x) {
+    x.style.width = "45%";
+    x.style.height = "35%";
+}
